1a9d8ca82d7931740de7a79a87d41a08c43c673e
Apurva-Sonare <apurvasona1692@gmail.com>
2024-09-30 13:23:31 +0530

    Added Login feature.

--- PatientManagement/Program.cs
+++ PatientManagement/Program.cs
@@ -1,3 +1,5 @@
+using PatientManagement.View;
+
 namespace PatientManagement
 {
     internal static class Program
@@ -11,7 +13,11 @@ namespace PatientManagement
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Master());
+            Login login = new Login();
+            if (login.ShowDialog() == DialogResult.OK)
+            {
+                Application.Run(new Master());
+            }
         }
     }
 }
